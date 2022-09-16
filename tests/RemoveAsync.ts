9145c341bbc6c9a5d5ba27a5885b8fc0aaf531e9
unknown <hi@devcomp.xyz>
2022-09-16 09:06:57 +0530

    feat: datastore RemoveAsync()

---
+++ tests/RemoveAsync.ts
@@ -0,0 +1,17 @@
+import { Datastore } from '../src'
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+
+//@ts-ignore
+const datastore = new Datastore(process.env.universeId, process.env.apiKey)
+
+
+export default async function main() {
+    console.log(await datastore.RemoveAsync("KeyStore", "TESTING"))
+}
+
+main()
+
