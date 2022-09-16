dd062c265c9abb73d99e77e157aca56b4fc4fc60
unknown <hi@devcomp.xyz>
2022-09-16 09:39:10 +0530

    fix: use proper apiKey in ListDataStoresAsync() test

--- tests/ListDataStoresAsync.ts
+++ tests/ListDataStoresAsync.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 //@ts-ignore
-const datastore = new Datastore(process.env.universeId, "k")
+const datastore = new Datastore(process.env.universeId, process.env.apiKey)
 
 
 export default async function main() {
